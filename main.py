a2c5311e6ec348ee6f469751bffa89d4c1ef3562
bigby <316828036@qq.com>
2020-07-14 18:02:16 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 from selenium import webdriver
 import time
 
+# id && pwd
 xmu_id = "id"
-xmu_pwd = "password"
+xmu_pwd = "pwd"
 
 driver = webdriver.Chrome()
 
+# login with id and pwd
 driver.get("https://ids.xmu.edu.cn/authserver/login?service=https://xmuxg.xmu.edu.cn/login/cas/xmu")
 driver.find_element_by_xpath('//input[@id="username"]').send_keys(xmu_id)
 driver.find_element_by_xpath('//input[@id="password"]').send_keys(xmu_pwd)
 driver.find_element_by_xpath('//button[@class="auth_login_btn primary full_width"]').click()
 
 driver.get("https://xmuxg.xmu.edu.cn/app/214")
-time.sleep(5)
+time.sleep(3)
 
+# 模拟点击，“我的表单” → “请选择” → “是” → “保存”
 driver.find_element_by_xpath('//div[@title="我的表单"]').click()
-time.sleep(5)
+time.sleep(3)
 
-driver.find_element_by_xpath('//div[@id="select_1582538939790"]/div').click()
-driver.find_element_by_xpath('//label[@title="是 Yes"]/span[2]').click()
+if driver.find_element_by_xpath('//div[@id="select_1582538939790"]/div/div/span').text != "是 Yes":
+    driver.find_element_by_xpath('//div[@id="select_1582538939790"]/div').click()
+    driver.find_element_by_xpath('//label[@title="是 Yes"]/span[2]').click()
+
 driver.find_element_by_xpath('//div[@id="pdfDomContent"]/following-sibling::span[1]/span').click()
+time.sleep(1)
+
+# 处理弹窗
+alert = driver.switch_to.alert
+alert.accept()
